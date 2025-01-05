09a12ece849532a01b242c470219bc0dacfc9375
Janarthanan-Gnanamurthy <janagnanam12@gmail.com>
2025-01-05 12:59:12 +0530

    added tailwindcss and daisyui

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -7,9 +7,9 @@ import { RouterLink, RouterView } from 'vue-router'
     <!-- <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" /> -->
 
     <div >
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+      <nav class="bg-blue-600 p-5 ">
+        <RouterLink to="/" class="btn mr-5">Home</RouterLink>
+        <RouterLink to="/about" class="btn">About</RouterLink>
       </nav>
     </div>
   </header>
